57b5b63952cd757f0087f15dd82f616b1a714fd1
Barry McAndrews <bmcandrews3@hotmail.com>
2020-04-29 22:09:35 -0400

    switch to DocumentClient for DynamoDb

--- functions/RestApiFunction/api.ts
+++ functions/RestApiFunction/api.ts
@@ -1,15 +1,14 @@
 import API from 'lambda-api';
-import {DynamoDB} from 'aws-sdk';
 import {v5 as uuidv5} from 'uuid';
 import {Room} from './models';
+import AWS from 'aws-sdk';
 
 const UUID_NAMESPACE = '031548bd-10e5-460f-89d4-915896e06f65';
 const ROOMS_TABLE_NAME = process.env.ROOMS_TABLE_NAME!;
-
-const endpoint = process.env.AWS_SAM_LOCAL ? 'http://localhost:8000' : undefined;
+const MESSAGES_TABLE_NAME = process.env.MESSAGES_TABLE_NAME!;
 
 const api = API({version: 'v1.0.0', base: 'v1'});
-const dynamoDB = new DynamoDB({endpoint: endpoint});
+const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
 api.use((req, res, next) => {
   res.cors({});
@@ -17,21 +16,21 @@ api.use((req, res, next) => {
 });
 
 api.get('/rooms', async (req) => {
-  const result = await dynamoDB.scan({TableName: ROOMS_TABLE_NAME}).promise();
-  return result.Items!.map(Room.fromItem);
+  const result = await dynamodb.scan({TableName: ROOMS_TABLE_NAME}).promise();
+  return result.Items!;
 });
 
 api.post('/rooms', async (req, resp) => {
-  const room = new Room({
+  const room: Room = {
     name: req.body.name,
     id: uuidv5(req.body.name, UUID_NAMESPACE),
     creator: req.requestContext.authorizer!.claims['cognito:username']
-  });
+  };
 
   try {
-    await dynamoDB.putItem({
+    await dynamodb.put({
       TableName: ROOMS_TABLE_NAME,
-      Item: room.toItem(),
+      Item: room,
       ConditionExpression: 'attribute_not_exists(id)'
     }).promise();
 
@@ -51,26 +50,37 @@ api.post('/rooms', async (req, resp) => {
 
 api.get('/rooms/:name', async (req) => {
     const id = uuidv5(req.params.name!, UUID_NAMESPACE);
-    return Room.fromItem((await dynamoDB.getItem({
+    return await dynamodb.get({
       TableName: ROOMS_TABLE_NAME,
-      Key: {
-        id: {S: id},
-      }
-    }).promise()).Item);
+      Key: {id: id},
+    }).promise();
 });
 
 api.delete('/rooms/:name', async (req) => {
   const id = uuidv5(req.params.name!, UUID_NAMESPACE);
   try {
-    await dynamoDB.deleteItem({
+    await dynamodb.delete({
       TableName: ROOMS_TABLE_NAME,
-      Key: {id: {S: id},}
+      Key: {id: id},
     }).promise();
     return {status: 'deleted'}
   } catch (e) {
     return e
   }
+});
 
+api.get('/rooms/:name/messages', async (req) => {
+  const id = uuidv5(req.params.name!, UUID_NAMESPACE);
+  const limit = Number(req.query.limit || 20) || 20;
+  return await dynamodb.query({
+    TableName: MESSAGES_TABLE_NAME,
+    KeyConditionExpression : 'roomId = :rid',
+    ExpressionAttributeValues : {
+      ':rid' : id,
+    },
+    ScanIndexForward: false,
+    Limit: limit,
+  }).promise();
 });
 
 export async function handler(event: any, context: any){
--- functions/RestApiFunction/models.ts
+++ functions/RestApiFunction/models.ts
@@ -1,33 +1,7 @@
+import {PutItemInputAttributeMap} from 'aws-sdk/clients/dynamodb';
 
-export class Room {
-  public id: string;
-  public name?: string;
-  public creator?: string;
-
-  constructor(props: RoomProps) {
-    Object.assign(this, props);
-  }
-
-  static fromItem(item: any): Room {
-    return new Room({
-        id: item.id.S,
-        name: item.name.S,
-        creator: item.creator.S,
-      }
-    );
-  }
-
-  toItem() {
-    return{
-      id: {S: this.id},
-      name: {S: this.name},
-      creator: {S: this.creator},
-    }
-  }
-}
-
-export interface RoomProps {
+export interface Room {
   id: string;
-  name?: string;
-  creator?: string;
+  name: string;
+  creator: string;
 }
